afa55de0d60bccabdceaa1ad677edd36c5397be8
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-10 22:28:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rpgkeeper2 Readme
+
+The rpgkeeper2 project is an omega-wf based web application.
+
+**TODO**: Write an actual readme.
---
+++ settings.js
@@ -0,0 +1,40 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Settings for the rpgkeeper2 application.
+//
+// @module settings.js
+//----------------------------------------------------------------------------------------------------------------------
+
+var connect = require('connect');
+var swig = require('swig');
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Enables omega-wf debugging helpers. Disable this for production!
+DEBUG = true;
+
+// Server settings
+listenAddress = "0.0.0.0";
+listenPort = 8080;
+
+// Used for secure sessions. This should be unique per omega-wf application.
+secret = "9799a2dde63b2850ca2aadee07bc4ad01db80e02415b52f8fe3a143500dc9d99f8d1bfff8b8b67de8ae3344ea97687cd";
+
+// Middleware
+middleware = [
+    // Standard connect middleware
+    connect.query(),
+
+    // Not required, but recommended for auth
+    connect.cookieParser(secret),
+    connect.session({
+        secret: secret,
+        key: 'sid'
+    })
+];
+
+// Configure swig
+swig.init({
+    root: './templates'
+});
+
+//----------------------------------------------------------------------------------------------------------------------
