3b4c27051d4025ca32bad4ce45a5b3eb3a69d80b
Christopher S. Case <chris.case@g33xnexus.com>
2013-05-13 19:55:53 -0500

    More model work.

--- models.js
+++ models.js
@@ -8,10 +8,28 @@ var db = require('omega-wf').db;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-db.define('User', {
-    id: { type: db.INTEGER, autoIncrement: true, primaryKey: true},
+var User = db.define('User', {
     name: db.STRING,
-    email: db.STRING
+    email: { type: db.STRING, validate: {isEmail: true} }
 });
+
+var System = db.define('System', {
+    name: db.STRING,
+    description: db.TEXT
+});
+
+var Character = db.define('Character', {
+    name: db.STRING,
+    portrait: db.STRING,
+    thumbnail: db.STRING
+});
+
+// Associations
+
+User.hasMany(Character);
+Character.belongsTo(User);
+
+System.hasMany(Character);
+Character.belongsTo(System);
 
 //----------------------------------------------------------------------------------------------------------------------
