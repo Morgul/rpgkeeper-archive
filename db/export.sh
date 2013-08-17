c96cbca5c7159f743690ac93f16c018c712e3f79
David H. Bronke <whitelynx@gmail.com>
2013-08-16 19:59:23 -0500

    Added 'cd' into script dir to export.sh.

--- db/export.sh
+++ db/export.sh
@@ -4,6 +4,7 @@ host=localhost
 port=27017
 db=dnd4e
 
+cd "$(dirname $0)"
 for c in `mongo --quiet $host:$port/$db --eval 'db.getCollectionNames()' | sed 's/,/ /g'`
 do
     mongoexport --host $host --port $port -d $db -c $c > $c.json
