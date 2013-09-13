ec5783df461492414991c2c2eb24127ca97bd097
Christopher S. Case <chris.case@g33xnexus.com>
2013-09-13 09:18:34 -0500

    Fixed dropbox support to use non-expiring links.

--- client/controllers.js
+++ client/controllers.js
@@ -253,9 +253,12 @@
                 {
                     $scope.$apply(function()
                     {
-                        console.log('file:', files[0]);
-                        $scope.newchar.thumbnail = files[0].link;
-                        $scope.newchar.portrait = files[0].link;
+                        // This is a little obnoxious. Dropbox does not support non-expiring direct links from their
+                        // chooser api, however, any file in dropbox can be directly linked to. The solution? Rewrite
+                        // the url. Thankfully their 'preview' url is almost exactly the same format as url we need.
+                        var link = files[0].link.replace('https://www.', 'https://dl.');
+                        $scope.newchar.thumbnail = link;
+                        $scope.newchar.portrait = link;
 
                         console.log('$scope.newchar:', $scope.newchar);
                     });
--- systems/dnd4e/js/controllers.js
+++ systems/dnd4e/js/controllers.js
@@ -218,13 +218,17 @@ function DnDCharCtrl($scope, $dialog, $timeout)
     $scope.chooseDropboxImage = function()
     {
         Dropbox.choose({
-            linkType: "direct",
+            linkType: "preview",
             extensions: ["images"],
             success: function(files)
             {
                 $scope.$apply(function()
                 {
-                    $scope.character.portrait = files[0].link;
+                    // This is a little obnoxious. Dropbox does not support non-expiring direct links from their
+                    // chooser api, however, any file in dropbox can be directly linked to. The solution? Rewrite
+                    // the url. Thankfully their 'preview' url is almost exactly the same format as url we need.
+                    var link = files[0].link.replace('https://www.', 'https://dl.');
+                    $scope.character.portrait = link;
                     $scope.socket.emit('update_character', $scope.character, function(error)
                     {
                         if(error)
