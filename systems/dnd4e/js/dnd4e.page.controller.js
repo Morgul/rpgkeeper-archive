45f6de2fd6f5b3fdbc25567a2ec8083092c60f09
Christopher S. Case <chris.case@g33xnexus.com>
2014-01-24 16:34:00 -0600

    Added feat directive

--- systems/dnd4e/js/dnd4e.page.controller.js
+++ systems/dnd4e/js/dnd4e.page.controller.js
@@ -348,48 +348,6 @@ module.controller('DnD4ePageCtrl', function($scope, $modal)
         });
     }; // end editFeat
 
-    $scope.editFeatRef = function(featRef, event) {
-        event.stopPropagation();
-
-        var opts = {
-            backdrop: 'static',
-            keyboard: true,
-            windowClass: "wide",
-            resolve: { featRef: function(){ return featRef } , editFeat: function(){ return $scope.editFeat; }},
-            templateUrl: '/systems/dnd4e/partials/modals/editfeatref.html',
-            controller: 'EditFeatRefModalCtrl'
-        };
-
-        $modal.open(opts).result.then(function(result)
-        {
-            if(result)
-            {
-                $scope.systemSocket.emit("update featRef", result, function(error, featRefRet)
-                {
-                    $scope.$apply(function()
-                    {
-                        _.assign(featRef, featRefRet);
-                    });
-                });
-            } // end if
-        });
-    }; // end editFeatRef
-
-    $scope.removeFeat = function(featRef, event)
-    {
-        // Prevent the event from triggering a collapse/expand event.
-        event.stopPropagation();
-
-        // Tell the system to remove the reference
-        $scope.systemSocket.emit("remove featRef", featRef.$id, $scope.sysChar.baseChar, function(error, character)
-        {
-            $scope.$apply(function()
-            {
-                $scope.sysChar = character;
-            });
-        });
-    }; // end removeFeat
-
     //------------------------------------------------------------------------------------------------------------------
     // Powers
     //------------------------------------------------------------------------------------------------------------------
---
+++ systems/dnd4e/widgets/feats/feats.js
@@ -0,0 +1,102 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Brief Description of feats.js.
+//
+// @module feats.js
+// ---------------------------------------------------------------------------------------------------------------------
+
+module.controller('FeatController', function($scope, $modal)
+{
+    console.log($scope.toggle);
+    $scope.featRef = $scope.featRef();
+
+    // Support pure feat objects
+    if($scope.featRef.name)
+    {
+        $scope.featRef = {
+            feat: $scope.featRef
+        };
+    } // end if
+
+    // Only disable this if it's explicitly set to false.
+    if($scope.editable != false)
+    {
+        $scope.editable = true;
+    } // end if
+
+    // Only disable this if it's explicitly set to false.
+    if($scope.removable != false)
+    {
+        $scope.removeable = true;
+    } // end if
+
+    $scope.collapseClick = function()
+    {
+        if($scope.toggle != undefined)
+        {
+            $scope.toggle = !$scope.toggle;
+        } // end if
+    };
+
+    $scope.editFeatRef = function(featRef, event) {
+        event.stopPropagation();
+
+        var opts = {
+            backdrop: 'static',
+            keyboard: true,
+            windowClass: "wide",
+            resolve: { featRef: function(){ return featRef } , editFeat: function(){ return $scope.editFeat; }},
+            templateUrl: '/systems/dnd4e/partials/modals/editfeatref.html',
+            controller: 'EditFeatRefModalCtrl'
+        };
+
+        $modal.open(opts).result.then(function(result)
+        {
+            if(result)
+            {
+                $scope.systemSocket.emit("update featRef", result, function(error, featRefRet)
+                {
+                    $scope.$apply(function()
+                    {
+                        _.assign(featRef, featRefRet);
+                    });
+                });
+            } // end if
+        });
+    }; // end editFeatRef
+
+    $scope.removeFeat = function(featRef, event)
+    {
+        // Prevent the event from triggering a collapse/expand event.
+        event.stopPropagation();
+
+        // Tell the system to remove the reference
+        $scope.systemSocket.emit("remove featRef", featRef.$id, $scope.sysChar.baseChar, function(error, character)
+        {
+            $scope.$apply(function()
+            {
+                $scope.sysChar = character;
+            });
+        });
+    }; // end removeFeat
+
+});
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+module.directive('feat', function()
+{
+    return {
+        restrict: 'E',
+        scope: {
+            toggle: "=",
+            featRef: "&",
+            editable: "@",
+            removable: "@"
+        },
+        templateUrl: '/systems/dnd4e/widgets/feats/feat.html',
+        controller: 'FeatController',
+        replace: true
+    };
+});
+
+// ---------------------------------------------------------------------------------------------------------------------
